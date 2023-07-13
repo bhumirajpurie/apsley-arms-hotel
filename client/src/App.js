efea04cbc2843d251457a2d9ccbb09f05e582c14
Rajesh limbu <rajeshlimbu355@gmail.com>
2023-07-13 11:05:39 +0545

    rajesh

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Product } from "./components/Product";
 import { Receipe } from "./components/Receipe";
-import { Accomodation } from "./components/Accomodation";
+import  Accomodation  from "./components/Accomodation";
 import { About } from "./components/About";
 import { Login } from "./components/Login";
 import { Shoppingcart } from "./components/Shoppingcart";
 import SignUp from "./components/Signup";
+import Registration from "./components/Registration";
+import FormInput from "./components/FormInput";
 function App() {
   return (
     <div className="App">
@@ -22,6 +24,8 @@ function App() {
           <Route path="/accomodation" element={<Accomodation />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/cart" element={<Shoppingcart />} />
+          <Route path="/Registration" element={(<Registration/>)}/>
+          <Route path="/FormInput" element={(<FormInput/>)}/>
         </Routes>
       </Router>
     </div>
